fcdf3a2e1ba098557eb71b893b4423580cac9113
teddyxlandlee <teddyxlandlee@hotmail.com>
2025-04-13 19:48:03 +0800

    [ci publish] 3.2.0: implement credits for translators on Fabric

---
+++ fabric/src/main/java/xland/mcmod/endpoemext/fabric/LangPatchCopyright.java
@@ -0,0 +1,86 @@
+package xland.mcmod.endpoemext.fabric;
+
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mojang.logging.LogUtils;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.GsonHelper;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import xland.mcmod.enchlevellangpatch.api.EnchantmentLevelLangPatch;
+import xland.mcmod.endpoemext.VanillaTextLocator;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public final class LangPatchCopyright implements SimpleSynchronousResourceReloadListener {
+    private static final ResourceLocation IDENTIFIER = ResourceLocation.fromNamespaceAndPath("end-poem-extension", "langpatch_copyright");
+    public static final Logger LOGGER = LogUtils.getLogger();
+    private static volatile Map<String, String> poemCredits = Collections.emptyMap();
+    private static final Gson GSON = new Gson();
+
+    @Override
+    public ResourceLocation getFabricId() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public void onResourceManagerReload(@Nonnull ResourceManager resourceManager) {
+        Map<ResourceLocation, Resource> map = resourceManager.listResources("texts/end_poem",
+                id -> "end_poem_extension".equals(id.getNamespace()) && id.getPath().endsWith(".metadata"));
+        Map<String, String> newMap = Maps.newHashMap();
+        for (Map.Entry<ResourceLocation, Resource> entry : map.entrySet()) {
+            final String langKey = StringUtils.removeEnd(entry.getKey().getPath(), ".metadata");
+            try (BufferedReader reader = entry.getValue().openAsReader()) {
+                newMap.put(langKey, IOUtils.toString(reader));
+            } catch (IOException e) {
+                LOGGER.warn("Failed to load poem translation metadata in language `{}`", langKey, e);
+            }
+        }
+        poemCredits = newMap;
+    }
+
+    private static @Nullable String readUri(String json) {
+        try {
+            JsonObject obj = GSON.fromJson(json, JsonObject.class);
+            return GsonHelper.getAsString(obj, "demo", null);
+        } catch (Throwable t) {
+            LOGGER.warn("Failed to parse credits json", t);
+            return null;
+        }
+    }
+
+    public static void load() {
+        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new LangPatchCopyright());
+        EnchantmentLevelLangPatch.registerPatch(
+                Predicate.isEqual("modmenu.descriptionTranslation.end-poem-extension"),
+                (translationStorage, key) -> {
+                    final String originTranslation = translationStorage.get(key);
+                    final Map<String, String> credits = poemCredits;
+                    Optional<String> o = Optional.ofNullable(credits.get(VanillaTextLocator.getLangCode()))
+                            .map(LangPatchCopyright::readUri);
+                    if (o.isEmpty()) {
+                        return originTranslation;
+                    }
+
+                    // Use I18n to allow API re-invoked
+                    String s = I18n.get("epx.poem.copyright.loaded_with", o.get());
+                    return originTranslation + s;
+                }
+        );
+    }
+}
